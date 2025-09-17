f8b17bcaa8d4f77f494dd48dc98831077968f01e
mkanderson2024 <kalani2014.ma@gmail.com>
2025-09-17 15:55:09 -0600

    saving work

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ app.set("layout", "layouts/layout") // not at views root
 // })
 
 // Index Route
-app.get("/", baseController.buildHome)
+app.get("/", utilities.handleErrors(baseController.buildHome))
 // Inventory Routes
 app.use("/inv", inventoryRoute)
 
@@ -43,12 +43,17 @@ app.use(async (req, res, next) => {
 * Express Error Handler
 * Place after all other middleware
 *************************/
+/* ***********************
+* Express Error Handler
+* Place after all other middleware
+*************************/
 app.use(async (err, req, res, next) => {
   let nav = await utilities.getNav()
   console.error(`Error at: "${req.originalUrl}": ${err.message}`)
+  if(err.status == 404){ message = err.message} else {message = 'Oh no! There was a crash. Maybe try a different route?'}
   res.render("errors/error", {
     title: err.status || 'Server Error',
-    message: err.message,
+    message,
     nav
   })
 })
