189a557d7eaa1edf6068176e66a8e1d425797a7f
mkanderson2024 <kalani2014.ma@gmail.com>
2025-09-17 12:38:09 -0600

    Fixing errors for error message

--- server.js
+++ server.js
@@ -12,6 +12,7 @@ const expressLayouts = require("express-ejs-layouts")
 const static = require("./routes/static")
 const baseController = require("./controllers/baseController")
 const inventoryRoute = require("./routes/inventoryRoute")
+const utilities = require("./utilities/");
 
 
 /* ***********************
@@ -34,9 +35,23 @@ app.use("/inv", inventoryRoute)
 
 app.use(express.static("public"))
 
+app.use(async (req, res, next) => {
+  next({status: 404, message: 'Sorry, we appear to have lost that page.'})
+})
 
-
-
+/* ***********************
+* Express Error Handler
+* Place after all other middleware
+*************************/
+app.use(async (err, req, res, next) => {
+  let nav = await utilities.getNav()
+  console.error(`Error at: "${req.originalUrl}": ${err.message}`)
+  res.render("errors/error", {
+    title: err.status || 'Server Error',
+    message: err.message,
+    nav
+  })
+})
 
 /* ***********************
  * Local Server Information
